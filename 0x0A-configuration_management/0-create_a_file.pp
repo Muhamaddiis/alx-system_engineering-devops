648d1eaefedd3825a705fef46fac02a6e2c55f53
Muhamaddiis <khalitiman17@gmail.com>
2023-08-27 04:52:13 -0700

    fixing-bugs

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-   #Create a file
+#Creates a file
 
   file { '/tmp/school':
-    ensure  => present,
+    ensure  => 'present',
     mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
     content => 'I love Puppet'
   }
-
