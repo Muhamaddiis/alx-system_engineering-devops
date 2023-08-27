7929a2d2aba916516cc1d69944afd54de033ba7b
Muhamaddiis <khalitiman17@gmail.com>
2023-08-27 04:29:33 -0700

    initial-commint

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+#install version (2.1.0) of flask using pip
+
+  package {'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+  }
+
+
+  package {'python3-pip':
+    ensure   => installed,
+  }
