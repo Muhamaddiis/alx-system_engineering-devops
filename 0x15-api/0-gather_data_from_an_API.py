60cecd6b5f8dd6a72103c9504663f7635f25b7ed
unknown <khalitiman17@gmail.com>
2024-04-22 12:30:18 +0300

    pycode

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 """Returns to-do list information for a given employee ID."""
-import sys
 import requests
+import sys
+
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com"
     employee_id = sys.argv[1]
-    
-    response = requests.get(f"{url}/users/{employee_id}")
-    data = response.json()
-    employee_name = data['name']
-    
+
+    users = requests.get(f"{url}/users/{employee_id}").json()
+
     response = requests.get(f"{url}/todos?userId={employee_id}")
     todos = response.json()
-    completed_tasks = [todo for todo in todos if todo['completed']]
-    
-    print(f"Employee {employee_name} is done with tasks "
-          f"({len(completed_tasks)}/{len(todos)}):")
-    
-    for task in completed_tasks:
-        print(f"\t{task['title']}")
+    completed_tasks = [todo.get("title") for todo in todos if todo.get("completed") is True]
+
+    print("Employee {} is done with tasks ({}/{}):".format(users.get("name"),len(completed_tasks), len(todos)))
+
+    [print("\t {}".format(c)) for c in completed_tasks]
+
